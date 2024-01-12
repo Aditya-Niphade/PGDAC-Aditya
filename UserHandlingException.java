c34c0254b980d15d63105cda97cd0e397c03cb8e
Aditya-Niphade <156420817+Aditya-Niphade@users.noreply.github.com>
2024-01-12 18:16:17 +0530

    Add files via upload

---
+++ UserHandlingException.java
@@ -0,0 +1,10 @@
+package com.cdac.DemoCRUD.customException;
+
+public class UserHandlingException extends RuntimeException{
+	public UserHandlingException(String msg) {
+		
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+
+}
